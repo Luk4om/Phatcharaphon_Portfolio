6ccc187fc950a1f7a1ecb37e4d83f8a3ab518eaf
Luk4om <halls12345ham@gmail.com>
2025-03-29 14:27:47 +0700

    fix for vercel

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,16 +1,16 @@
 "use client"
 
-import Button from "@/components/ui/Button"
-import Input from "@/components/ui/Input"
-import TextArea from "@/components/ui/Textarea"
+// import Button from "@/components/ui/Button"
+// import Input from "@/components/ui/Input"
+// import TextArea from "@/components/ui/Textarea"
 
-import {
-  Select,
-  SelectTrigger,
-  SelectContent,
-  SelectItem,
-  SelectValue
-} from "@/components/ui/select";
+// import {
+//   Select,
+//   SelectTrigger,
+//   SelectContent,
+//   SelectItem,
+//   SelectValue
+// } from "@/components/ui/select";
 
 import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa"
 import { motion } from "framer-motion"
@@ -44,21 +44,18 @@ const Contact = () => {
       className="py-6">
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
-          {/* form */}
-          <div className="xl:w-[54%] order-2 xl:order-none">
+          {/* <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-amber-400">Let's Work Together</h3>
               <p className="text-white/60">
                 Sample form for submitting details.
               </p>
-              {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <input type="text" placeholder="Firstname" className="bg-transparent border rounded-lg p-4 outline-none focus:border-amber-400 border-white/20 h-[52px] text-white" />
                 <input type="text" placeholder="Lastname" className="bg-transparent border rounded-lg p-4 outline-none focus:border-amber-400 border-white/20 h-[52px] text-white" />
                 <input type="email" placeholder="Email Address" className="bg-transparent border rounded-lg p-4 outline-none focus:border-amber-400 border-white/20 h-[52px] text-white" />
                 <input type="tel" placeholder="Phone Number" className="bg-transparent border rounded-lg p-4 outline-none focus:border-amber-400 border-white/20 h-[52px] text-white" />
               </div>
-              {/* select */}
               <Select>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Select a subject" />
@@ -72,31 +69,27 @@ const Contact = () => {
                   </SelectGroup>
                 </SelectContent>
               </Select>
-              {/* textarea */}
               <TextArea className="h-[200px]" placeholder="Type your Message here." />
-              {/* button */}
               <Button size="md" className="max-w-40">
                 Send Message
               </Button>
             </form>
-          </div>
+          </div> */}
           {/* info */}
-          <div className="flex flex-1 items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            <ul className="flex flex-col gap-10">
+          <div className="flex flex-1 items-start justify-center order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10 items-start">
               {info.map((item, index) => {
                 return (
-                  <li
-                    key={index}
-                    className="flex items-center gap-6">
+                  <li key={index} className="flex items-start gap-6">
                     <div className="w-[52px] h-[52px] xl:h-[72px] bg-[#27272c] text-amber-400 rounded-md flex items-center justify-center">
                       <div className="text-[28px]">{item.icon}</div>
                     </div>
-                    <div className="flex-1">
+                    <div className="flex-1 text-start">
                       <p className="text-white/60">{item.title}</p>
                       <h3 className="text-xl">{item.description}</h3>
                     </div>
                   </li>
-                )
+                );
               })}
             </ul>
           </div>
